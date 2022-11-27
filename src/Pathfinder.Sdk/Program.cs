85461769901adc1742761126673227e2c7be61af
Justin Cook <justin-cook@pluralsight.com>
2022-11-27 14:09:18 -0500

    buildConfig method and user secrets

--- src/Pathfinder.Sdk/Program.cs
+++ src/Pathfinder.Sdk/Program.cs
@@ -14,15 +14,34 @@ namespace Pathfinder.Sdk
         {
             Console.WriteLine("Hello World");
 
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true)
-                .Build();
+            var configuration = BuildConfiguration();
+            // var configuration = new ConfigurationBuilder()
+            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //     .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true)
+            //     .Build();
 
+            // var token = configuration.GetRequiredSection("Discord");
             // var pf2eBot = new Pf2eBot();
             // pf2eBot.Run().GetAwaiter().GetResult();
 
             Console.WriteLine("Goodbye Cruel World");
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var env = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            var isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true);
+            
+            if(isDevelopment)
+            {
+                builder.AddUserSecrets<Program>();
+            }
+
+            return builder.Build();
         }
     }
 }
