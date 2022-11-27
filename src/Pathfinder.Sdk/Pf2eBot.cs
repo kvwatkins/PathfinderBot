7b7f80a0e81296cd4a66c12f224936dc5164b697
Justin Cook <justin-cook@pluralsight.com>
2022-11-27 15:06:23 -0500

    bot interface and di

--- src/Pathfinder.Sdk/Pf2eBot.cs
+++ src/Pathfinder.Sdk/Pf2eBot.cs
@@ -1,15 +1,18 @@
 using Discord;
 using Discord.WebSocket;
 using System.Threading.Tasks;
+using Models.Configuration;
 
 namespace Pathfinder.Sdk
 {
-    internal class Pf2eBot
+    internal class Pf2eBot : IPf2eBot
     {
         private readonly DiscordSocketClient _client;
+        private readonly Models.Configuration.DiscordConfig _config;
 
-        public Pf2eBot()
+        public Pf2eBot(Models.Configuration.DiscordConfig config)
         {
+            _config = config;
             _client = new DiscordSocketClient();
         }
 
@@ -18,9 +21,7 @@ namespace Pathfinder.Sdk
             //_client.Log += Log;
             //_client.MessageReceived += ClientOnMessageReceived;
 
-            var token = "Your Token Here";
-
-            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.LoginAsync(TokenType.Bot, _config.Bot.Token);
             await _client.StartAsync();
 
             // Block this task until the program is closed.
--- src/Pathfinder.Sdk/Program.cs
+++ src/Pathfinder.Sdk/Program.cs
@@ -4,27 +4,40 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.File;
 using System;
+using Models.Configuration;
 
 
 namespace Pathfinder.Sdk
 {
     class Program
     {
+        public static IConfigurationRoot Configuration {get; set;}
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
 
-            var configuration = BuildConfiguration();
-            // var configuration = new ConfigurationBuilder()
-            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //     .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true)
-            //     .Build();
+            Configuration = BuildConfiguration();
+
+            var services = BuildServiceProvider();
 
             // var token = configuration.GetRequiredSection("Discord");
             // var pf2eBot = new Pf2eBot();
             // pf2eBot.Run().GetAwaiter().GetResult();
 
             Console.WriteLine("Goodbye Cruel World");
+        }
+
+        private static ServiceProvider BuildServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services
+                .Configure<DiscordConfig>(c => Configuration.GetRequiredSection("discordConfig").Bind(c))
+                .AddOptions()
+                .BuildServiceProvider();
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
         }
 
         private static IConfigurationRoot BuildConfiguration()
